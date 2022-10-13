86f0829b6eced770a7642978b869856f08139701
virani keval <70562450+VIRANIKEVAL@users.noreply.github.com>
2022-10-14 00:20:56 +0530

    updated salary file
    Hello, i have made some changes to your file please check.

--- salary.py
+++ salary.py
@@ -1,31 +1,13 @@
-print ("MENU")
-print ()
-print ("B - Burger ")
-print("F - French Fries")
-print("P - Pizza ")
-print("S - Sandwich")
-print ()
-Burger= 0
-FrenchFries= 0
-Pizza= 0
-Sandwich= 0
 
-x=1
-
-while x==1:
-    i = input("Enter the food that you want to buy: ")
-    if i  == "B":
-        Burger+=200
-        print ("Burger ------------Php. 200.00")
-    elif i == "F" :
-        FrenchFries+=50
-        print ("FrenchFries ------------Php. 50.00")
-    elif i == "P":
-         Pizza+=500
-         print ("Pizza ------------Php. 500.00")
-    elif i == "S":
-         Sandwich+=150
-         print ("Sandwich ---------Php. 150.00")
-    x=int(input("Do you still want to buy anything? 1 if yes 0 if no "))
-total= Burger + FrenchFries + Pizza + Sandwich   
-print("Total: ", total)
+def getGrossPay(annual_salary, no_of_pay_periods):
+    return float(annual_salary/no_of_pay_periods)
+ 
+# driver code
+ 
+ 
+# annual_salary in lakhs
+annual_salary = 12
+no_of_pay_periods = 12
+pay = getGrossPay(annual_salary, no_of_pay_periods)
+ 
+print(f"Total gross pay: Rs.{pay:.2f} lakhs ")
