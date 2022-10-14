b92a22b63cc2e6711c04949345f2670f56861b2e
Yash <65134112+YashPatel-15@users.noreply.github.com>
2022-10-15 04:39:44 +0530

    updated fibo file
    Hello, i have made changes to your fie please check and review.

--- fibo.py
+++ fibo.py
@@ -1,13 +1,25 @@
-def fibo(n):
-    if(n <= 1):
-        return n
-    else:
-        return(fibo(n-1) + fibo(n-2))
+# Program to display the Fibonacci sequence up to n-th term
 
-number = 10
+nterms = int(input("How many terms? "))
 
-if(number <= 0):
-    print("enter positive number")
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+# generate fibonacci sequence
 else:
-    for i in range(number):
-        print(fibo(i))
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
