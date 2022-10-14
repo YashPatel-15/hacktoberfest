acebd1245a8e92b0ae58d92f74321cb50c843bf0
VIRANIKEVAL25 <91781475+VIRANIKEVAL25@users.noreply.github.com>
2022-10-15 04:59:03 +0530

    updated gui file
    Hello, i have made changes to your fie please check and review.

--- gui.py
+++ gui.py
@@ -1,25 +1,37 @@
-from tkinter import *
-from tkinter.ttk import Combobox			#import tkinter library
-
-window = Tk()
-
-v0 = IntVar()
-v0.set(1)
-r1 = Radiobutton(window, text="male", variable=v0, value=1)
-r2 = Radiobutton(window, text="female", variable=v0, value=2)
-r1.place(x=100, y=50)
-r2.place(x=180, y=50)
-
-v1 = IntVar()
-v2 = IntVar()
-C1 = Checkbutton(window, text="Cricket", variable=v1)
-C2 = Checkbutton(window, text="Tennis", variable=v2)
-C1.place(x=100, y=100)
-C2.place(x=180, y=100)
-
-window.title('Hello Python')
-window.geometry("400x300+10+10")
-window.mainloop()
-
-
-
+# A Python program to demonstrate inheritance
+ 
+# Base or Super class. Note object in bracket.
+# (Generally, object is made ancestor of all classes)
+# In Python 3.x "class Person" is
+# equivalent to "class Person(object)"
+ 
+ 
+class Person(object):
+ 
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+ 
+    # To get name
+    def getName(self):
+        return self.name
+ 
+    # To check if this person is an employee
+    def isEmployee(self):
+        return False
+ 
+ 
+# Inherited or Subclass (Note Person in bracket)
+class Employee(Person):
+ 
+    # Here we return true
+    def isEmployee(self):
+        return True
+ 
+ 
+# Driver code
+emp = Person("Geek1")  # An Object of Person
+print(emp.getName(), emp.isEmployee())
+ 
+emp = Employee("Geek2")  # An Object of Employee
+print(emp.getName(), emp.isEmployee())
